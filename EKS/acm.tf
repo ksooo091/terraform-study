5ec3612287c8206c001b3c42e6852c0cd3ac96e1
KimGilSu <hwanrim00@gmail.com>
2023-04-07 19:25:32 +0900

    [fix] del acm

--- EKS/acm.tf
+++ EKS/acm.tf
@@ -6,12 +6,3 @@ resource "aws_acm_certificate" "cert_wildCard" {
     create_before_destroy = true
   }
 }
-
-resource "aws_acm_certificate" "cert_non_wildCard" {
-  domain_name       = var.DOMAIN
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
