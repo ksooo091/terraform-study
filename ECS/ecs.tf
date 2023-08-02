7a204e274effee232b006d9626887a5ea5d4103d
ksooo091 <hwanrim00@gmail.com>
2023-08-02 20:46:58 +0900

    [modify] ECS service modify

--- ECS/ecs.tf
+++ ECS/ecs.tf
@@ -27,26 +27,29 @@ resource "aws_ecs_task_definition" "study_task" {
 }
 
 
-# resource "aws_ecs_service" "study_service" {
-#   name            = "nginx"
-#   cluster         = aws_ecs_cluster.study_cluster.id
-#   task_definition = aws_ecs_task_definition.study_task.arn
-#   desired_count   = 1
-
-#   network_configuration {
-#     security_groups  = [aws_security_group.ecs_sg.id]
-#     subnets          = [aws_subnet.ecs_public_a.id,
-#                         aws_subnet.ecs_public_c.id]
-#  #   assign_public_ip =  true
-#   }
 
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.ecs_lb_tg.arn
-#     container_name   = "nginx"
-#     container_port   = 80
-#   }
+resource "aws_ecs_service" "study_service" {
+  name            = "nginx"
+  cluster         = aws_ecs_cluster.study_cluster.id
+  task_definition = aws_ecs_task_definition.study_task.arn
+  desired_count   = 1
+    launch_type     = "FARGATE"
 
-# }
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets          = [aws_subnet.ecs_public_a.id,
+                        aws_subnet.ecs_public_c.id]
+    assign_public_ip =  true
+  }
+
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_lb_tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
+}
 
 
