5018866fe4040b2f9e952e7201657415f82f1106
ksooo091 <hwanrim00@gmail.com>
2023-08-04 19:08:41 +0900

    [feat] module

---
+++ module/vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
