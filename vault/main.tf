8a6da26bb41896f8e0c5f93607c9d53f39ab378b
KimGilSu <hwanrim00@gmail.com>
2023-11-07 18:03:11 +0900

    vault

---
+++ vault/main.tf
@@ -0,0 +1,13 @@
+# Specify the app in the project (one app_name per data block)
+data "hcp_vault_secrets_app" "aws_app" {
+  app_name = "testapp"
+# Limit the scope to only one or more secrets in the app
+# secret_name = “secret-name-1” “secret-name-2”
+}
+
+# Replace your existing secret references with
+# data.hcp-vault-secrets_app.aws_app.secret-name
+
+output "vault" {
+  value = data.hcp_vault_secrets_app.aws_app
+}
